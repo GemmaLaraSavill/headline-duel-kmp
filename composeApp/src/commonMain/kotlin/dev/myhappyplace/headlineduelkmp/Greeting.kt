e41fe3a43e5e52af564824e2668753872cc203ff
GLS <gemmalarasav@gmail.com>
2025-11-25 15:25:03 +0100

    chore: Remove unused KMP template files and cleanup

--- composeApp/src/commonMain/kotlin/dev/myhappyplace/headlineduelkmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.myhappyplace.headlineduelkmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
